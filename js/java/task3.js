fad05c3f7e4568e0587b5a6234da23737f77dcc4
Johannes Lindblom <johannes.lind@hotmail.se>
2024-05-15 11:03:36 +0200

    lol

--- js/java/task3.js
+++ js/java/task3.js
@@ -1,17 +1,37 @@
 document.getElementById('check-1');
+var temp1 = 2;
+const dices = [0, 0, 0, 0, 0];
 
-document.querySelector('#throwDice').addEventListener('click',function(){
-    var num = 1;
-    const nodeList = document.querySelectorAll(".dice");
-    for (let i = 0; i < nodeList.length; i++) {
-        
-        var t1 = Math.floor(Math.random() * 6) +1;
-        if (document.getElementById("check-" + num).checked != true)
-        {
-            nodeList[i].src = '/js/img/dice-' + t1 + '.png';
-            console.log("off");
-        }
-        console.log(num);
-        num++;
+
+document.getElementById('throwDice').addEventListener('click',function(){
+    document.getElementById('counter').innerHTML = temp1--;
+    if(document.getElementById('counter').innerHTML == 0){
+        document.getElementById('throwDice').disabled = true;
+        document.getElementById('throwDice').style.background = '#995f08';
+        document.getElementById('throwDice').style.color = '#969087';
     }
+    
+        var num = 1;
+        const nodeList = document.querySelectorAll(".dice");
+        for (let i = 0; i < nodeList.length; i++) {
+            
+            
+            if (document.getElementById("check-" + (i + 1)).checked != true)
+            {
+                var t1 = Math.floor(Math.random() * 6) +1;
+                nodeList[i].src = '/js/img/dice-' + t1 + '.png';
+                nodeList[i].alt = t1;
+                console.log("off");
+            }
+            dices[i] = nodeList[i].alt;
+            console.log(dices);
+            console.log(i);
+            
+        }
+    
+
+});
+document.querySelector('#point').addEventListener('click', function(){
+    
+
 });
